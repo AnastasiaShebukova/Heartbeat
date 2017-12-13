7c8ccb5b9c05037b02afa1d10b05e4eeeaf5fd74
LeanaNeuber <LeanaNeuber@github.com>
2017-12-13 14:03:19 +0100

    Neue Anpassungen in Absprache mit Lochi

--- src/org/htw/fiw/vs/implementation/HeartBeatImpl.java
+++ src/org/htw/fiw/vs/implementation/HeartBeatImpl.java
@@ -4,51 +4,56 @@ import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.htw.fiw.vs.interfaces.HeartBeat;
-import org.htw.fiw.vs.interfaces.HeartBeatObserverInterface;
-import org.htw.fiw.vs.interfaces.HeartBeatSubjectInterface;
+import rmi.interfaces.IHeartBeatObserver;
+import rmi.interfaces.IHeartBeatSubject;
 
-public class HeartBeatImpl extends java.rmi.server.UnicastRemoteObject implements HeartBeat, HeartBeatObserverInterface, HeartBeatSubjectInterface  {
+public class HeartBeatImpl extends java.rmi.server.UnicastRemoteObject implements IHeartBeatSubject {
 
-	int heartbeat;
-	
-	List<Object> observerList = new ArrayList<Object>();
-	
+	private static HeartBeatImpl instance = null;
+
 	protected HeartBeatImpl() throws RemoteException {
 		super();
-		// TODO Auto-generated constructor stub
 	}
-	
+
+	public static HeartBeatImpl getInstance() throws RemoteException {
+		if (instance == null) {
+			instance = new HeartBeatImpl();
+		}
+		return instance;
+	}
+
+	private static final long serialVersionUID = 1L;
+	int heartbeat;
+
+	List<IHeartBeatObserver> observerList = new ArrayList<IHeartBeatObserver>();
+
 	@Override
 	public int getHeartBeat() throws RemoteException {
 		return this.heartbeat;
 	}
-	
-	//die funktion k�nnte von dem REST call aufgerufen werden? Und w�rde dann den state setzen und die beaobachter benachrichtigen?
+
+	// die funktion k�nnte von dem REST call aufgerufen werden? Und w�rde dann den
+	// state setzen und die beaobachter benachrichtigen?
 	public void setHeartBeat(int heartbeat) throws RemoteException {
 		this.heartbeat = heartbeat;
 		notifyObservers();
 	}
-	
-	//das ist unklar. Die update Methode m�sste doch eigentlich auf der anderen Seite implementiert werden, und nicht beide Interfaces in einer Klasse.
+
 	@Override
-	public void update(Object observer) throws RemoteException {
+	public void subscribeObject(IHeartBeatObserver observer) throws RemoteException {
+		this.observerList.add(observer);
 	}
-	
+
 	@Override
-	public void subscribeObject(Object observer) throws RemoteException {
-		this.observerList.add(observer);		
-	}
-	
-	@Override
-	public void removeObject(Object observer) throws RemoteException {
+	public void removeObject(IHeartBeatObserver observer) throws RemoteException {
 		this.observerList.remove(observer);
-		
+
 	}
+
 	@Override
 	public void notifyObservers() throws RemoteException {
-		for (Object observer : this.observerList) {
-			update(observer);
+		for (IHeartBeatObserver observer : this.observerList) {
+			observer.update(this.heartbeat);
 		}
 	}
 }
--- src/org/htw/fiw/vs/interfaces/HeartBeat.java
+++
@@ -1,8 +0,0 @@
-package org.htw.fiw.vs.interfaces;
-
-public interface HeartBeat extends java.rmi.Remote {
-	
-	public int getHeartBeat()
-			throws java.rmi.RemoteException;
-
-}
